a8d36fd3244048e4f8f4b5c7d64af024fced2e3f
Harish Sheoran <www.harishsheoran@gmail.com>
2022-07-28 15:23:12 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 document.querySelector('#invert').addEventListener('click', function(){
     let elem = document.getElementById('intro');
     let elem2 = document.getElementById('info');
-    elem.classList.remove("fs-5");
     elem.classList.remove("text-muted");
-    elem2.classList.remove("fs-5");
     elem2.classList.remove("text-muted");
     document.body.classList.toggle("invert");
 });
